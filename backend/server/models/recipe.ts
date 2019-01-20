3f84d2465cb68d9b638022d720f73272c73dbfd4
jeffreyrampineda <jeffreyrampineda@gmail.com>
2019-01-20 14:42:37 -0500

    added nutrition facts

--- backend/server/models/recipe.ts
+++ backend/server/models/recipe.ts
@@ -1,6 +1,8 @@
 import * as mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
+
+// TODO: redesign to reduce payload.
 const recipeSchema = new Schema({
     title: {
         type: String,
@@ -15,7 +17,30 @@ const recipeSchema = new Schema({
     },
     tags: [{ type: String }],
     ingredients: [{ name: String, quantity: Number }],
-    steps: [{ type: String }]
+    steps: [{ type: String }],
+    nutritionFacts: {
+        servingsPerContainer: { type: Number },
+        quantity: { type: Number },
+        units: { type: Number },
+        size: { type: Number },
+// -------------------------------------------------------------
+        calories: { type: Number },
+// -------------------------------------------------------------
+        fats: {
+            total: Number,
+            saturated: Number,
+            trans: Number
+        },
+        cholesterol: { type: Number },
+        sodium: { type: Number },
+        carbohydrates: {
+            total: Number,
+            dietaryFiber: Number,
+            sugars: Number,
+            addedSugars: Number,
+        },
+        protein: { type: Number },
+    }
 });
 
 export default mongoose.model('Recipe', recipeSchema);
--- frontend/src/app/interfaces/recipe.ts
+++ frontend/src/app/interfaces/recipe.ts
@@ -6,4 +6,27 @@ export interface Recipe {
     ingredients: { name: string, quantity: number }[];
     steps: string[];
     // TODO: include nutrition facts.
+    nutritionFacts: {
+        servingsPerContainer: number;
+        quantity: number;
+        units: number;
+        size: number;
+// -------------------------------------------------------------
+        calories: number;
+// -------------------------------------------------------------
+        fats: {
+            total: number,
+            saturated: number,
+            trans: number
+        };
+        cholesterol: number;
+        sodium: number;
+        carbohydrates: {
+            total: number,
+            dietaryFiber: number,
+            sugars: number,
+            addedSugars: number,
+        };
+        protein: number;
+    };
 }
