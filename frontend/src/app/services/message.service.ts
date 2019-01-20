f9bc6853f49ef630042f4d8f1bddb01ebd9b3eea
jeffreyrampineda <jeffreyrampineda@gmail.com>
2019-01-19 23:45:04 -0500

    centralized handleError

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 // -------------------------------------------------------------
 
@@ -23,5 +24,26 @@ export class MessageService {
   /** Clears the messages array. */
   clear() {
     this.messages = [];
+  }
+
+  /**
+   * TODO: Check security - private vs public.
+   *
+   * Error handler used for any http errors.
+   * @param operation - The type of operation used.
+   * @param result - The results received.
+   */
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.add(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
--- frontend/src/app/services/recipe.service.ts
+++ frontend/src/app/services/recipe.service.ts
@@ -51,17 +51,7 @@ export class RecipeService {
    * @param result - The results received.
    */
   private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+    return this.messageService.handleError<T>(operation, result);
   }
 
   /**
