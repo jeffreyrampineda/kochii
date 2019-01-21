d7eccaffcc84751d2280808e1e986cd7583c28a6
jeffreyrampineda <jeffreyrampineda@gmail.com>
2019-01-20 22:04:27 -0500

    fixed fragment jump

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'kochii-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.fragment.subscribe(f => {
+      const element = document.querySelector('#' + f);
+      if (element) {
+        element.scrollIntoView();
+      }
+    });
   }
-
 }
