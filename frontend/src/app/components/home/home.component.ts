7537f393ad0332d4f8e4975f0744ffab55a74d58
jeffreyrampineda <jeffreyrampineda@gmail.com>
2019-07-03 01:32:15 -0400

    improved style

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,23 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
+// -------------------------------------------------------------
 
 @Component({
   selector: 'kochii-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      state('hide',   style({
+        opacity: 0,
+        transform: 'translateY(-100%)'
+      })),
+      transition('show => hide', animate('300ms ease-out')),
+      transition('hide => show', animate('300ms ease-in'))
+    ])
+  ],
 })
 export class HomeComponent implements OnInit {
 
+  isLoggedIn = false;
+  state = 'hide';
+
   constructor(
-    private activatedRoute: ActivatedRoute
+    private authenticationService: AuthenticationService,
   ) { }
 
+// -------------------------------------------------------------
+
   ngOnInit() {
-    this.activatedRoute.fragment.subscribe(f => {
-      const element = document.querySelector('#' + f);
-      if (element) {
-        element.scrollIntoView();
-      }
-    });
+    this.isLoggedIn = this.authenticationService.currentUserValue ? true : false;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const topSection = window.innerHeight;
+    const scrollPosition = window.pageYOffset;
+
+    if (scrollPosition >= topSection) {
+      this.state = 'show';
+    } else {
+      this.state = 'hide';
+    }
+  }
+
+  /**
+   * Logs out the current user.
+   */
+  logout(): void {
+    this.authenticationService.logout();
+  }
+
+  /**
+   * Scrolls to the specified section.
+   * @param section - The id or class of the section.
+   */
+  goto(section: string): void {
+    const element = document.querySelector(section);
+
+    element.scrollIntoView();
   }
 }
