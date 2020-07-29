c7a3ccbe4c7a20bba822d863e9f7f90a40913cfd
jeffrey ram <jeffreyrampineda@gmail.com>
2020-07-29 12:35:52 -0400

    fixed minor: jest mongodb testing

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 // Set up database connection before any tests
 beforeAll(async () => {
-    await mongoose.connect(process.env.MONGODB_URI_local, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
 // close database and server after all tests are complete
@@ -14,49 +14,58 @@ afterAll(async () => {
 });
 
 describe('/api route', () => {
-    test('GET /api/inventory without token should give Authentication Error', async () => {
+    test('GET /api/inventory without token should give Unauthorized', async () => {
         const response = await request(server).get('/api/inventory');
 
         expect(response.status).toEqual(401);
-        expect(response.text).toContain('Authentication Error');
+        expect(response.text).toContain('Unauthorized');
     });
-    test('GET /api/groups without token should give Authentication Error', async () => {
+    test('GET /api/groups without token should give Unauthorized', async () => {
         const response = await request(server).get('/api/groups');
 
         expect(response.status).toEqual(401);
-        expect(response.text).toContain('Authentication Error');
+        expect(response.text).toContain('Unauthorized');
     });
-    test('GET /api/history without token should give Authentication Error', async () => {
+    test('GET /api/history without token should give Unauthorized', async () => {
         const response = await request(server).get('/api/history');
 
         expect(response.status).toEqual(401);
-        expect(response.text).toContain('Authentication Error');
+        expect(response.text).toContain('Unauthorized');
     });
 });
 
 describe('/public route', () => {
-     test('POST /public/login with correct auth should give 202 status with token object', async () => {
-        const user = {
-            username: "tester",
-            password: "tester"
-        }
+    const correct_user = {
+        username: "correct_username",
+        password: "correct_password",
+        email: "correct_email@correct.com"
+    }
 
+    test('POST /public/register with correct data should give 202 status /w token', async () => {
+        const response = await request(server)
+            .post('/public/register')
+            .send(correct_user);
+
+        expect(response.status).toEqual(202);
+        expect(response.text).toContain('token');
+    });
+    test('POST /public/login with correct data should give 202 status /w token', async () => {
         const response = await request(server)
             .post('/public/login')
-            .send(user);
+            .send(correct_user);
 
         expect(response.status).toEqual(202);
         expect(response.text).toContain('token');
     });
     test('POST /public/login with incorrect auth should give 401 status', async () => {
-        const user = {
-            username: "tester",
-            password: "wrong_password"
+        const incorrect_user = {
+            username: "incorrect_usename",
+            password: "incorrect_password"
         }
 
         const response = await request(server)
             .post('/public/login')
-            .send(user);
+            .send(incorrect_user);
 
         expect(response.status).toEqual(401);
         expect(response.text).toContain('Authentication failed');
