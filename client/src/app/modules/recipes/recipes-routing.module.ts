0532a826285b1085bb8fcbe51bee30ee4b5e0bd8
jeffrey ram pineda <jeffreyrampineda@gmail.com>
2022-04-30 01:24:41 -0400

    refactor: renamed components->views

--- client/src/app/modules/recipes/recipes-routing.module.ts
+++ client/src/app/modules/recipes/recipes-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PostListComponent } from './components/post-list/post-list.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { PostCookComponent } from './components/post-cook/post-cook.component';
-import { CollectionsListComponent } from './components/collections-list/collections-list.component';
+import { PostListComponent } from './views/post-list/post-list.component';
+import { PostDetailComponent } from './views/post-detail/post-detail.component';
+import { PostCookComponent } from './views/post-cook/post-cook.component';
+import { CollectionsListComponent } from './views/collections-list/collections-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'catalog', pathMatch: 'full' },
--- client/src/app/modules/recipes/recipes.module.ts
+++ client/src/app/modules/recipes/recipes.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { RecipesRoutingModule } from './recipes-routing.module';
-import { PostListComponent } from './components/post-list/post-list.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { PostCookComponent } from './components/post-cook/post-cook.component';
-import { CollectionsListComponent } from './components/collections-list/collections-list.component';
+import { PostListComponent } from './views/post-list/post-list.component';
+import { PostDetailComponent } from './views/post-detail/post-detail.component';
+import { PostCookComponent } from './views/post-cook/post-cook.component';
+import { CollectionsListComponent } from './views/collections-list/collections-list.component';
 
 @NgModule({
   declarations: [
